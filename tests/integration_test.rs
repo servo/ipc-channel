ab286d569e5e6f13e6db4fd099adb7943e59638b
Glyn Normington <glyn.normington@gmail.com>
2025-01-24 14:20:54 +0000

    Add integration test
    Fixes https://github.com/servo/ipc-channel/issues/382

---
+++ tests/integration_test.rs
@@ -0,0 +1,41 @@
+// Copyright 2025 The Servo Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use ipc_channel::ipc::IpcOneShotServer;
+use std::{env, process};
+
+// These integration tests may be run on their own by issuing:
+// cargo test --test '*'
+
+/// Test spawing a process which then acts as a client to a
+/// one-shot server in the parent process.
+#[test]
+fn spawn_one_shot_server_client() {
+    let executable_path: String = env!("CARGO_BIN_EXE_spawn_client_test_helper").to_string();
+
+    let (server, token) =
+        IpcOneShotServer::<String>::new().expect("Failed to create IPC one-shot server.");
+
+    let mut command = process::Command::new(executable_path);
+    let child_process = command.arg(token);
+
+    let mut child = child_process
+        .spawn()
+        .expect("Failed to start child process");
+
+    let (_rx, msg) = server.accept().expect("accept failed");
+    assert_eq!("test message", msg);
+
+    let result = child.wait().expect("wait for child process failed");
+    assert!(
+        result.success(),
+        "child process failed with exit status code {}",
+        result.code().expect("exit status code not available")
+    );
+}
