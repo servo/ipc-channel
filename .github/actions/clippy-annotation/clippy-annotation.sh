7a65a47bea90b94f11fce1485a6eb7cca594f7f0
Jerens Lensun <jerensslensun@gmail.com>
2025-08-05 16:18:03 +0800

    clippy: add changes based on PR review
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- .github/actions/clippy-annotation/clippy-annotation.sh
+++ .github/actions/clippy-annotation/clippy-annotation.sh
@@ -4,10 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-jq -r '
+annotations=$(jq -r '
   select(.reason == "compiler-message")
   | .message as $msg
   | select($msg.level == "error" or $msg.level == "warning")
   | $msg.spans[]
   | select(.is_primary)
-  | "::\($msg.level) file=\(.file_name),line=\(.line_start),col=\(.column_start)\(if .column_end != .column_start then ",endColumn=\(.column_end)" else "" end)::\($msg.message)"'
+  | "::\($msg.level) file=\(.file_name),line=\(.line_start),col=\(.column_start)\(if .column_end != .column_start then ",endColumn=\(.column_end)" else "" end)::\($msg.message)"
+')
+
+# Print the annotations
+if [[ -n "$annotations" ]]; then
+  echo "$annotations"
+  exit 1
+fi
