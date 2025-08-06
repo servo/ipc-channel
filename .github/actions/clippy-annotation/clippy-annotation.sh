569400def0434dc6c8c46cf93a7368aeee4280a7
Jerens Lensun <jerensslensun@gmail.com>
2025-08-06 14:40:00 +0800

    clippy: make clippy not cancel if fail
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- .github/actions/clippy-annotation/clippy-annotation.sh
+++ .github/actions/clippy-annotation/clippy-annotation.sh
@@ -4,17 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-annotations=$(jq -r '
+jq -r '
   select(.reason == "compiler-message")
   | .message as $msg
   | select($msg.level == "error" or $msg.level == "warning")
   | $msg.spans[]
   | select(.is_primary)
-  | "::\($msg.level) file=\(.file_name),line=\(.line_start),col=\(.column_start)\(if .column_end != .column_start then ",endColumn=\(.column_end)" else "" end)::\($msg.message)"
-')
-
-# Print the annotations
-if [[ -n "$annotations" ]]; then
-  echo "$annotations"
-  exit 1
-fi
+  | "::\($msg.level) file=\(.file_name),line=\(.line_start),col=\(.column_start)\(if .column_end != .column_start then ",endColumn=\(.column_end)" else "" end)::\($msg.message)"'
