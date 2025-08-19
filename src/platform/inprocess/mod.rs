d49050ae0c584ee74d9073a5a39a315ba02178f1
Jerens Lensun <jerensslensun@gmail.com>
2025-08-19 17:24:31 +0800

    script_bindings(python): Add comment Safety on unsafe OsIpcSharedMemory
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- src/platform/inprocess/mod.rs
+++ src/platform/inprocess/mod.rs
@@ -359,6 +359,10 @@ impl Deref for OsIpcSharedMemory {
 
 impl OsIpcSharedMemory {
     /// # Safety
+    ///
+    /// This is safe if there is only one reader/writer on the data.
+    /// User can achieve this by not cloning [`IpcSharedMemory`]
+    /// and serializing/deserializing only once.
     #[inline]
     pub unsafe fn deref_mut(&mut self) -> &mut [u8] {
         if self.ptr.is_null() {
