9574384c6e97f99110e4aa54e0ce90778f0b9876
Josh Matthews <josh@joshmatthews.net>
2025-12-16 09:59:46 -0500

    Elide lifetime.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- src/platform/macos/mod.rs
+++ src/platform/macos/mod.rs
@@ -378,7 +378,7 @@ impl<'a> From<&'a [u8]> for SendData<'a> {
     }
 }
 
-impl<'a> SendData<'a> {
+impl<'_> SendData<'_> {
     fn take_shared_memory(&mut self) -> Option<OsIpcSharedMemory> {
         match *self {
             SendData::Inline(_) => None,
