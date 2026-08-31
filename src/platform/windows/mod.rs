0d0e90b81e8ff3ef4ebeaa35cf21eb07b61f3242
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-31 06:50:51 +0200

    clippy fixes
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -1482,7 +1482,13 @@ impl OsIpcSender {
             full_message.extend_from_slice(header_bytes);
         }
 
-        if big_data_sender.is_none() {
+        if let Some(big_data_sender) = big_data_sender {
+            full_message.extend_from_slice(&oob_data);
+            assert!(full_message.len() == full_in_band_len);
+
+            write_buf(&self.handle, &full_message, AtomicMode::Atomic)?;
+            big_data_sender.send_raw(data)?;
+        } else {
             full_message.extend_from_slice(data);
             full_message.extend_from_slice(&oob_data);
             assert!(full_message.len() == full_in_band_len);
@@ -1491,12 +1497,6 @@ impl OsIpcSender {
             // could result in parts of different messages getting intermixed,
             // and the receiver would not be able to extract the individual messages.
             write_buf(&self.handle, &full_message, AtomicMode::Atomic)?;
-        } else {
-            full_message.extend_from_slice(&oob_data);
-            assert!(full_message.len() == full_in_band_len);
-
-            write_buf(&self.handle, &full_message, AtomicMode::Atomic)?;
-            big_data_sender.unwrap().send_raw(data)?;
         }
 
         Ok(())
